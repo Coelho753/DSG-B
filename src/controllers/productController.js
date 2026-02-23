e8c0621568168018cdd19eec0461ec98ca60ecb2
Coelho753 <165965908+Coelho753@users.noreply.github.com>
2026-02-23 18:40:01 -0300

    Atualizar o productController.js

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,52 +1,26 @@
 const Product = require("../models/Product");
 
-const createProduct = async (req, res) => {
+exports.createProduct = async (req, res) => {
   try {
-    const product = await Product.create(req.body);
+    const product = new Product({
+      name: req.body.name,
+      price: req.body.price,
+      description: req.body.description,
+      image: req.file ? `/uploads/${req.file.filename}` : "",
+    });
+
+    await product.save();
     res.status(201).json(product);
   } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Erro ao criar produto" });
+    res.status(500).json({ error: error.message });
   }
 };
 
-const getProducts = async (req, res) => {
+exports.getProducts = async (req, res) => {
   try {
     const products = await Product.find();
     res.json(products);
   } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Erro ao buscar produtos" });
+    res.status(500).json({ error: error.message });
   }
-};
-
-const updateProduct = async (req, res) => {
-  try {
-    const product = await Product.findByIdAndUpdate(
-      req.params.id,
-      req.body,
-      { new: true }
-    );
-    res.json(product);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Erro ao atualizar produto" });
-  }
-};
-
-const deleteProduct = async (req, res) => {
-  try {
-    await Product.findByIdAndDelete(req.params.id);
-    res.json({ message: "Produto deletado" });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Erro ao deletar produto" });
-  }
-};
-
-module.exports = {
-  createProduct,
-  getProducts,
-  updateProduct,
-  deleteProduct
 };
