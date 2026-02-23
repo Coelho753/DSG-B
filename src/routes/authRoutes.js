dea1e251f31fdeab43d52f80c905cb6dbe6f8e45
Coelho753 <165965908+Coelho753@users.noreply.github.com>
2026-02-23 20:49:47 -0300

    Atualizar o authRoutes.js

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,25 +1,8 @@
-/**
- * Routes: mapeia endpoints HTTP para seus respectivos controllers e middlewares.
- * Arquivo: src/routes/authRoutes.js
- */
-const express = require('express');
-const authController = require('../controllers/authController');
-const authMiddleware = require('../middlewares/authMiddleware');
-const authorizeRoles = require('../middlewares/roleMiddleware');
-const validateRequest = require('../middlewares/validateRequest');
-const {
-  registerValidator,
-  loginValidator,
-  refreshTokenValidator,
-  bootstrapAdminValidator,
-} = require('../validators/authValidator');
-
+const express = require("express");
 const router = express.Router();
+const authController = require("../controllers/authController");
 
-router.post('/register', registerValidator, validateRequest, authController.register);
-router.post('/bootstrap-admin', bootstrapAdminValidator, validateRequest, authController.bootstrapAdmin);
-router.post('/register/admin', authMiddleware, authorizeRoles('admin'), registerValidator, validateRequest, authController.register);
-router.post('/login', loginValidator, validateRequest, authController.login);
-router.post('/refresh-token', refreshTokenValidator, validateRequest, authController.refreshToken);
+router.post("/login", authController.login);
+router.post("/register", authController.register);
 
 module.exports = router;
