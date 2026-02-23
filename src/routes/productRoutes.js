612398f12ebe61269f52cd1e8c22c5ad8140b30d
Coelho753 <165965908+Coelho753@users.noreply.github.com>
2026-02-23 18:40:22 -0300

    Atualizar o productRoutes.js

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -1,11 +1,20 @@
 const express = require("express");
-const productController = require("../controllers/productController");
-
 const router = express.Router();
+const multer = require("multer");
+const path = require("path");
 
-router.get("/", productController.getProducts);
-router.post("/", productController.createProduct);
-router.put("/:id", productController.updateProduct);
-router.delete("/:id", productController.deleteProduct);
+const { createProduct, getProducts } = require("../controllers/productController");
+
+const storage = multer.diskStorage({
+  destination: "./src/uploads",
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + path.extname(file.originalname));
+  },
+});
+
+const upload = multer({ storage });
+
+router.post("/", upload.single("image"), createProduct);
+router.get("/", getProducts);
 
 module.exports = router;
