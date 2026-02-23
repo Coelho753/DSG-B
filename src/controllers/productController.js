748f190d453264aadac882292a7670d59ec79360
Coelho753 <165965908+Coelho753@users.noreply.github.com>
2026-02-23 20:51:37 -0300

    Atualizar o productController.js

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -2,25 +2,10 @@ const Product = require("../models/Product");
 
 exports.createProduct = async (req, res) => {
   try {
-    const product = new Product({
-      name: req.body.name,
-      price: req.body.price,
-      description: req.body.description,
-      image: req.file ? `/uploads/${req.file.filename}` : "",
-    });
-
-    await product.save();
+    const product = await Product.create(req.body);
     res.status(201).json(product);
   } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-};
-
-exports.getProducts = async (req, res) => {
-  try {
-    const products = await Product.find();
-    res.json(products);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
+    console.error(error);
+    res.status(500).json({ message: "Erro ao criar produto" });
   }
 };
