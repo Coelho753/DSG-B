02cef66907dda593483311bf313716abb7b37262
Coelho753 <165965908+Coelho753@users.noreply.github.com>
2026-02-23 18:39:04 -0300

    Atualizar o Promotion.js

--- src/models/Promotion.js
+++ src/models/Promotion.js
@@ -1,24 +1,15 @@
 const mongoose = require("mongoose");
 
-const promotionSchema = new mongoose.Schema(
-  {
-    product: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Product",
-      required: true
-    },
-    discountPercentage: {
-      type: Number,
-      required: true,
-      min: 1,
-      max: 100
-    },
-    active: {
-      type: Boolean,
-      default: true
-    }
+const promotionSchema = new mongoose.Schema({
+  product: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Product",
+    required: true,
   },
-  { timestamps: true }
-);
+  discount: {
+    type: Number,
+    required: true,
+  },
+});
 
 module.exports = mongoose.model("Promotion", promotionSchema);
