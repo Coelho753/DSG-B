0405ef9b13890f6e2276bc583ea17a6200b3b060
Coelho753 <165965908+Coelho753@users.noreply.github.com>
2026-02-24 19:59:10 -0300

    Atualizar o Cart.js

--- src/models/Cart.js
+++ src/models/Cart.js
@@ -2,11 +2,21 @@ const mongoose = require("mongoose");
 
 const cartSchema = new mongoose.Schema(
   {
-    user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
     items: [
       {
-        product: { type: mongoose.Schema.Types.ObjectId, ref: "Product" },
-        quantity: Number,
+        product: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Product",
+        },
+        quantity: {
+          type: Number,
+          default: 1,
+        },
       },
     ],
   },
