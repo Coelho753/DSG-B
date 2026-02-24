fc10d773e99253e25c1601522ecc70048713fbac
Coelho753 <165965908+Coelho753@users.noreply.github.com>
2026-02-24 16:30:53 -0300

    Atualizar o server.js

--- src/server.js
+++ src/server.js
@@ -1,40 +1,29 @@
+require("dotenv").config();
 const express = require("express");
-const cors = require("cors");
-const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
-dotenv.config();
+const authRoutes = require("./routes/authRoutes");
+const productRoutes = require("./routes/productRoutes");
+const categoryRoutes = require("./routes/categoryRoutes");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// IMPORTA ROTAS
-const authRoutes = require("./routes/authRoutes");
-const userRoutes = require("./routes/userRoutes");
-const productRoutes = require("./routes/productRoutes");
-const promotionRoutes = require("./routes/promotionRoutes");
-const paymentRoutes = require("./routes/paymentRoutes");
-const cartRoutes = require("./routes/cartRoutes");
-const categoryRoutes = require("./routes/categoryRoutes");
-
-
-
-// USA ROTAS
-app.use("/api/auth", authRoutes);
-app.use("/api/users", userRoutes);
-app.use("/api/products", productRoutes);
-app.use("/api/promotions", promotionRoutes);
-app.use("/api/payments", paymentRoutes);
-app.use("/uploads", express.static("uploads"));
-app.use("/api/cart", cartRoutes);
-app.use("/api/categories", categoryRoutes);
-
-
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB conectado"))
-  .catch(err => console.error(err));
+  .catch(err => console.log(err));
+
+app.use("/api/auth", authRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/categories", categoryRoutes);
+
+app.get("/", (req, res) => {
+  res.send("API funcionando");
+});
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
+
+app.listen(PORT, () => console.log("Servidor rodando"));
