f65ac56acff55a4e23da97fa5f969fc0bff58476
Coelho753 <165965908+Coelho753@users.noreply.github.com>
2026-02-23 21:09:52 -0300

    Atualizar o productController.js

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,5 +1,6 @@
 const Product = require("../models/Product");
 
+// Criar
 exports.createProduct = async (req, res) => {
   try {
     const product = await Product.create(req.body);
@@ -7,5 +8,62 @@ exports.createProduct = async (req, res) => {
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Erro ao criar produto" });
+  }
+};
+
+// Listar
+exports.getProducts = async (req, res) => {
+  try {
+    const products = await Product.find();
+    res.json(products);
+  } catch (error) {
+    res.status(500).json({ message: "Erro ao buscar produtos" });
+  }
+};
+
+// Buscar por ID
+exports.getProductById = async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    if (!product) {
+      return res.status(404).json({ message: "Produto não encontrado" });
+    }
+    res.json(product);
+  } catch (error) {
+    res.status(500).json({ message: "Erro ao buscar produto" });
+  }
+};
+
+// Atualizar
+exports.updateProduct = async (req, res) => {
+  try {
+    const product = await Product.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+
+    if (!product) {
+      return res.status(404).json({ message: "Produto não encontrado" });
+    }
+
+    res.json(product);
+  } catch (error) {
+    res.status(500).json({ message: "Erro ao atualizar produto" });
+  }
+};
+
+// Deletar
+exports.deleteProduct = async (req, res) => {
+  try {
+    const product = await Product.findByIdAndDelete(req.params.id);
+
+    if (!product) {
+      return res.status(404).json({ message: "Produto não encontrado" });
+    }
+
+    res.json({ message: "Produto removido com sucesso" });
+  } catch (error) {
+    res.status(500).json({ message: "Erro ao deletar produto" });
   }
 };
