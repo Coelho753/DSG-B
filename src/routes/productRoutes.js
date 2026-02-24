31f162f47a0e356a6929383b5d95978962e18434
Coelho753 <165965908+Coelho753@users.noreply.github.com>
2026-02-24 17:48:42 -0300

    Atualizar o productRoutes.js

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -1,25 +1,24 @@
 const express = require("express");
 const router = express.Router();
-const multer = require("multer");
-const storage = multer.memoryStorage();
-const upload = multer({ storage });
-
 
 const {
   getProducts,
   getProductById,
   createProduct,
-  deleteProduct
+  deleteProduct,
 } = require("../controllers/productController");
 
 const auth = require("../middlewares/authMiddleware");
 const admin = require("../middlewares/adminMiddleware");
 
+const multer = require("multer");
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
+
 router.get("/", getProducts);
 router.get("/:id", getProductById);
 
 router.post("/", auth, admin, upload.single("image"), createProduct);
-
 router.delete("/:id", auth, admin, deleteProduct);
 
 module.exports = router;
