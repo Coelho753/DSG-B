ff406db0ec9c2938fbb166bae8820bca9129dde4
Coelho753 <165965908+Coelho753@users.noreply.github.com>
2026-02-24 17:42:05 -0300

    Atualizar o cartRoutes.js

--- src/routes/cartRoutes.js
+++ src/routes/cartRoutes.js
@@ -1,10 +1,20 @@
 const express = require("express");
 const router = express.Router();
-const cartController = require("../controllers/cartController");
-const authMiddleware = require("../middlewares/authMiddleware");
+const auth = require("../middlewares/authMiddleware");
+const Cart = require("../models/Cart");
 
-router.get("/", authMiddleware, cartController.getCart);
-router.post("/", authMiddleware, cartController.addToCart);
-router.delete("/", authMiddleware, cartController.removeFromCart);
+router.get("/", auth, async (req, res) => {
+  try {
+    let cart = await Cart.findOne({ user: req.user._id }).populate("items.product");
+
+    if (!cart) {
+      cart = await Cart.create({ user: req.user._id, items: [] });
+    }
+
+    res.json(cart);
+  } catch {
+    res.status(500).json({ message: "Erro ao buscar carrinho" });
+  }
+});
 
 module.exports = router;
