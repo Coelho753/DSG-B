d208c67299fc85f1789484af8698a5aa9fbc5e3e
Coelho753 <165965908+Coelho753@users.noreply.github.com>
2026-02-24 16:29:42 -0300

    Atualizar o productController.js

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,69 +1,22 @@
 const Product = require("../models/Product");
+const cloudinary = require("../config/cloudinary");
 
-// Criar
 exports.createProduct = async (req, res) => {
   try {
-    const product = await Product.create(req.body);
+    const { name, price, description, category } = req.body;
+
+    const result = await cloudinary.uploader.upload(req.file.path);
+
+    const product = await Product.create({
+      name,
+      price,
+      description,
+      category,
+      image: result.secure_url,
+    });
+
     res.status(201).json(product);
   } catch (error) {
-    console.error(error);
     res.status(500).json({ message: "Erro ao criar produto" });
-  }
-};
-
-// Listar
-exports.getProducts = async (req, res) => {
-  try {
-    const products = await Product.find();
-    res.json(products);
-  } catch (error) {
-    res.status(500).json({ message: "Erro ao buscar produtos" });
-  }
-};
-
-// Buscar por ID
-exports.getProductById = async (req, res) => {
-  try {
-    const product = await Product.findById(req.params.id);
-    if (!product) {
-      return res.status(404).json({ message: "Produto não encontrado" });
-    }
-    res.json(product);
-  } catch (error) {
-    res.status(500).json({ message: "Erro ao buscar produto" });
-  }
-};
-
-// Atualizar
-exports.updateProduct = async (req, res) => {
-  try {
-    const product = await Product.findByIdAndUpdate(
-      req.params.id,
-      req.body,
-      { new: true }
-    );
-
-    if (!product) {
-      return res.status(404).json({ message: "Produto não encontrado" });
-    }
-
-    res.json(product);
-  } catch (error) {
-    res.status(500).json({ message: "Erro ao atualizar produto" });
-  }
-};
-
-// Deletar
-exports.deleteProduct = async (req, res) => {
-  try {
-    const product = await Product.findByIdAndDelete(req.params.id);
-
-    if (!product) {
-      return res.status(404).json({ message: "Produto não encontrado" });
-    }
-
-    res.json({ message: "Produto removido com sucesso" });
-  } catch (error) {
-    res.status(500).json({ message: "Erro ao deletar produto" });
   }
 };
