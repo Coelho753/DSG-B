fc5e434f647aff88bd74b4dc724243770afa05dd
Coelho753 <165965908+Coelho753@users.noreply.github.com>
2026-02-24 17:35:18 -0300

    Atualizar o Product.js

--- src/models/Product.js
+++ src/models/Product.js
@@ -1,35 +1,17 @@
-/**
- * Model: define o schema/estrutura persistida no MongoDB via Mongoose.
- * Arquivo: src/models/Product.js
- */
 const mongoose = require("mongoose");
 
 const productSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
-    description: { type: String, required: true },
-
-    imageUrl: {
-      type: String,
-      required: true
+    price: { type: Number, required: true },
+    description: String,
+    image: String,
+    category: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Category",
     },
-
-    price: {
-      type: Number,
-      required: true
-    },
-
-    stock: {
-      type: Number,
-      default: -1 // -1 = estoque infinito
-    },
-
-    soldCount: {
-      type: Number,
-      default: 0
-    }
   },
   { timestamps: true }
-)
+);
 
 module.exports = mongoose.model("Product", productSchema);
