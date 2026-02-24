de2157c6721e577e31bcfa963fc73f9edc6a8a5f
Coelho753 <165965908+Coelho753@users.noreply.github.com>
2026-02-23 21:47:41 -0300

    Atualizar o cartRoutes.js

--- src/routes/cartRoutes.js
+++ src/routes/cartRoutes.js
@@ -1,20 +1,10 @@
-/**
- * Routes: mapeia endpoints HTTP para seus respectivos controllers e middlewares.
- * Arquivo: src/routes/cartRoutes.js
- */
-const express = require('express');
-const authMiddleware = require('../middlewares/authMiddleware');
-const cartController = require('../controllers/cartController');
-const orderController = require('../controllers/orderController');
-
+const express = require("express");
 const router = express.Router();
+const cartController = require("../controllers/cartController");
+const authMiddleware = require("../middlewares/authMiddleware");
 
-router.use(authMiddleware);
-router.get('/', cartController.getCart);
-router.post('/add', cartController.addToCart);
-router.put('/update', cartController.updateCartItem);
-router.delete('/remove', cartController.removeCartItem);
-router.delete('/clear', cartController.clearCart);
-router.post('/checkout', orderController.checkoutCart);
+router.get("/", authMiddleware, cartController.getCart);
+router.post("/", authMiddleware, cartController.addToCart);
+router.delete("/", authMiddleware, cartController.removeFromCart);
 
 module.exports = router;
