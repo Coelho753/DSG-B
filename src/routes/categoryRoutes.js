77847f481185e26f3ce6f18440bac3190e357368
Coelho753 <165965908+Coelho753@users.noreply.github.com>
2026-02-23 21:46:45 -0300

    Atualizar o categoryRoutes.js

--- src/routes/categoryRoutes.js
+++ src/routes/categoryRoutes.js
@@ -1,23 +1,9 @@
-/**
- * Routes: mapeia endpoints HTTP para seus respectivos controllers e middlewares.
- * Arquivo: src/routes/categoryRoutes.js
- */
-const express = require('express');
-const categoryController = require('../controllers/categoryController');
-const authMiddleware = require('../middlewares/authMiddleware');
-const authorizeRoles = require('../middlewares/roleMiddleware');
-const validateRequest = require('../middlewares/validateRequest');
-const { categoryValidator } = require('../validators/categoryValidator');
-
+const express = require("express");
 const router = express.Router();
+const categoryController = require("../controllers/categoryController");
+const authMiddleware = require("../middlewares/authMiddleware");
 
-// Público: listagem e detalhe
-router.get('/', categoryController.getCategories);
-router.get('/:id', categoryController.getCategoryById);
-
-// Privado (admin): gestão
-router.post('/', authMiddleware, authorizeRoles('admin'), categoryValidator, validateRequest, categoryController.createCategory);
-router.put('/:id', authMiddleware, authorizeRoles('admin'), categoryValidator, validateRequest, categoryController.updateCategory);
-router.delete('/:id', authMiddleware, authorizeRoles('admin'), categoryController.deleteCategory);
+router.get("/", categoryController.getCategories);
+router.post("/", authMiddleware, categoryController.createCategory);
 
 module.exports = router;
