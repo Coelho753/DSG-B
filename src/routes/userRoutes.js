d5403751493745e8e1c703ec8efeb1e00a825c07
Coelho753 <165965908+Coelho753@users.noreply.github.com>
2026-02-23 21:07:57 -0300

    Atualizar o userRoutes.js

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
-const { getProfile, updateProfile } = require("../controllers/userController");
+const userController = require("../controllers/userController");
 const authMiddleware = require("../middlewares/authMiddleware");
 
+// Perfil do usuário logado
 router.get("/profile", authMiddleware, userController.getProfile);
+
+// Atualizar perfil
 router.put("/profile", authMiddleware, userController.updateProfile);
 
 module.exports = router;
