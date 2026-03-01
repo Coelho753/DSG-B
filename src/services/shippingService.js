dee7ba4b12c95293edcb0e124190579ea494a4a7
Coelho753 <165965908+Coelho753@users.noreply.github.com>
2026-03-01 16:10:36 -0300

    Atualizar o shippingService.js

--- src/services/shippingService.js
+++ src/services/shippingService.js
@@ -1,13 +1,40 @@
-/**
- * Service: concentra regras de negócio reutilizáveis e integrações externas.
- * Arquivo: src/services/shippingService.js
- */
-const calculateShipping = (subtotal) => {
-  const numericSubtotal = Number(subtotal || 0);
-  if (numericSubtotal >= 149) return 0;
-  return 19.9;
-};
+const axios = require("axios");
 
-module.exports = {
-  calculateShipping,
+const baseURL = process.env.MELHOR_ENVIO_BASE;
+
+const api = axios.create({
+  baseURL,
+  headers: {
+    Authorization: `Bearer ${process.env.MELHOR_ENVIO_TOKEN}`,
+    "Content-Type": "application/json",
+  },
+});
+
+exports.calculateShipping = async ({ zipcode, products }) => {
+  try {
+
+    const response = await api.post("/me/shipment/calculate", {
+      from: {
+        postal_code: "01001000", // CEP da sua loja
+      },
+      to: {
+        postal_code: zipcode,
+      },
+      products: products.map(p => ({
+        id: p._id,
+        width: p.width,
+        height: p.height,
+        length: p.length,
+        weight: p.weight,
+        insurance_value: p.price,
+        quantity: p.quantity,
+      })),
+    });
+
+    return response.data;
+
+  } catch (error) {
+    console.error("Erro cálculo frete:", error.response?.data || error);
+    throw new Error("Erro ao calcular frete");
+  }
 };
