bca43eed1de6ff38f8cd5fb55fc9d5d28740f4ee
Coelho753 <165965908+Coelho753@users.noreply.github.com>
2026-03-02 09:07:20 -0300

    Atualizar o paymentController.js

--- src/controllers/paymentController.js
+++ src/controllers/paymentController.js
@@ -1,18 +1,45 @@
-const { createPreference } = require("../services/mercadoPagoService");
+const axios = require("axios");
 
 exports.createPayment = async (req, res) => {
   try {
-    const { items } = req.body;
+    const {
+      token,
+      payment_method_id,
+      issuer_id,
+      amount,
+      installments,
+      email,
+    } = req.body;
 
-    const result = await createPreference(items);
+    const response = await axios.post(
+      "https://api.mercadopago.com/v1/payments",
+      {
+        transaction_amount: Number(amount),
+        token,
+        description: "Compra DSG",
+        installments: Number(installments),
+        payment_method_id,
+        issuer_id,
+        payer: {
+          email,
+        },
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.MP_ACCESS_TOKEN}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
 
     res.json({
-      id: result.id,
-      init_point: result.init_point,
+      status: response.data.status,
+      status_detail: response.data.status_detail,
+      id: response.data.id,
     });
 
   } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: "Erro ao criar pagamento" });
+    console.error(error.response?.data || error.message);
+    res.status(500).json({ message: "Erro ao processar pagamento" });
   }
 };
