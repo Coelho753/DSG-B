b217824e472b607fd4145ca62626845d5e4603fb
Coelho753 <165965908+Coelho753@users.noreply.github.com>
2026-03-04 09:25:47 -0300

    Atualizar o User.js

--- src/models/User.js
+++ src/models/User.js
@@ -1,14 +1,35 @@
 const mongoose = require("mongoose");
 
+const addressSchema = new mongoose.Schema({
+  street: { type: String, required: true },
+  number: { type: String, required: true },
+  neighborhood: { type: String, required: true },
+  city: { type: String, required: true },
+  state: { type: String, required: true },
+  zipCode: { type: String, required: true },
+  complement: String,
+});
+
 const userSchema = new mongoose.Schema(
   {
     name: String,
     email: { type: String, unique: true },
     password: String,
+
     role: {
       type: String,
       enum: ["user", "admin"],
       default: "user",
+    },
+
+    cpf: {
+      type: String,
+      required: true,
+    },
+
+    address: {
+      type: addressSchema,
+      required: true, // 🔥 obrigatório
     },
   },
   { timestamps: true }
