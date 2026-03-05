1dc0329e975b524b0930cfdd0e9784d535dd2aaa
Coelho753 <165965908+Coelho753@users.noreply.github.com>
2026-03-05 06:55:52 -0300

    Atualizar o orderRoutes.js

--- src/routes/orderRoutes.js
+++ src/routes/orderRoutes.js
@@ -3,6 +3,8 @@ const router = express.Router();
 
 const orderController = require("../controllers/orderController");
 
+router.get("/my-orders", authMiddleware, getMyOrders);
+
 router.post("/", orderController.createOrder);
 
 module.exports = router;
