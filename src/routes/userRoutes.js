f2434c8803dc74da70ffacccc481aeefbf954dc1
Coelho753 <165965908+Coelho753@users.noreply.github.com>
2026-03-06 07:34:36 -0300

    Atualizar o userRoutes.js

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,9 +1,58 @@
 const express = require("express");
 const router = express.Router();
-const auth = require("../middlewares/authMiddleware");
+const authMiddleware = require("../middlewares/authMiddleware");
+const User = require("../models/User");
 
-router.get("/me", auth, async (req, res) => {
-  res.json(req.user);
+router.get("/me", authMiddleware, async (req, res) => {
+  try {
+
+    const user = await User.findById(req.user.id)
+      .select("-password");
+
+    res.json(user);
+
+  } catch (error) {
+    res.status(500).json({ message: "Erro ao buscar usuário" });
+  }
+});
+
+
+router.put("/me", authMiddleware, async (req, res) => {
+
+  try {
+
+    const { name, email, address } = req.body;
+
+    const user = await User.findById(req.user.id);
+
+    if (!user) {
+      return res.status(404).json({ message: "Usuário não encontrado" });
+    }
+
+    if (name) user.name = name;
+    if (email) user.email = email;
+
+    if (address) {
+      user.address = address;
+    }
+
+    await user.save();
+
+    res.json({
+      message: "Usuário atualizado",
+      user
+    });
+
+  } catch (error) {
+
+    console.error(error);
+
+    res.status(500).json({
+      message: "Erro ao atualizar usuário"
+    });
+
+  }
+
 });
 
 module.exports = router;
