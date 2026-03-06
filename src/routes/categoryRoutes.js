7e3466d3d83cde57a026944c4efe86821d44719a
Coelho753 <165965908+Coelho753@users.noreply.github.com>
2026-03-06 07:38:04 -0300

    Atualizar o categoryRoutes.js

--- src/routes/categoryRoutes.js
+++ src/routes/categoryRoutes.js
@@ -4,6 +4,38 @@ const categoryController = require("../controllers/categoryController");
 const authMiddleware = require("../middlewares/authMiddleware");
 
 router.get("/", categoryController.getCategories);
-router.post("/", authMiddleware, categoryController.createCategory);
+router.post("/", authMiddleware, async (req, res) => {
 
+  try {
+
+    const { name } = req.body;
+
+    if (!name) {
+      return res.status(400).json({
+        message: "Nome obrigatório"
+      });
+    }
+
+    const slug = name
+      .toLowerCase()
+      .replace(/\s+/g, "-");
+
+    const category = await Category.create({
+      name,
+      slug
+    });
+
+    res.status(201).json(category);
+
+  } catch (error) {
+
+    console.error(error);
+
+    res.status(500).json({
+      message: "Erro ao criar categoria"
+    });
+
+  }
+
+});
 module.exports = router;
