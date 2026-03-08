9ede2f14a873e44ca1d6101d06bc09a8fa936616
Coelho753 <165965908+Coelho753@users.noreply.github.com>
2026-03-08 19:40:44 -0300

    Atualizar o checkoutRoutes.js

--- src/routes/checkoutRoutes.js
+++ src/routes/checkoutRoutes.js
@@ -1,15 +1,12 @@
 const express = require("express");
-
 const router = express.Router();
-
-const auth = require("../middlewares/authMiddleware");
 
 const checkoutController = require("../controllers/checkoutController");
 
-router.post(
-  "/",
-  auth,
-  checkoutController.createCheckout
-);
+/*
+CRIAR PEDIDO
+*/
+
+router.post("/create-order", checkoutController.createOrder);
 
 module.exports = router;
