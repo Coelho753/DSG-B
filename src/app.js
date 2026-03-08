1e515d9a5012e0ef64e4215d8a0d9f5d00fd3f48
Coelho753 <165965908+Coelho753@users.noreply.github.com>
2026-03-08 20:00:16 -0300

    Atualizar o app.js

--- src/app.js
+++ src/app.js
@@ -8,13 +8,29 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Servir uploads
+// uploads
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
-app.use("/api/shipping", require("./routes/shippingRoutes"));
-app.use("/api/products", require("./routes/productRoutes"));
-app.use("/api/promotions", require("./routes/promotionRoutes"));
+// ROTAS API
+
+app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/users", require("./routes/userRoutes"));
+
+app.use("/api/products", require("./routes/productRoutes"));
+app.use("/api/categories", require("./routes/categoryRoutes"));
+
+app.use("/api/cart", require("./routes/cartRoutes"));
+
+app.use("/api/coupons", require("./routes/couponRoutes"));
+
+app.use("/api/checkout", require("./routes/checkoutRoutes"));
+
+app.use("/api/orders", require("./routes/orderRoutes"));
+
 app.use("/api/payments", require("./routes/paymentRoutes"));
+
+app.use("/api/shipping", require("./routes/shippingRoutes"));
+
+app.use("/api/promotions", require("./routes/promotionRoutes"));
 
 module.exports = app;
