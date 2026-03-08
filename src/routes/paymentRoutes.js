9c698cf5ded3a92bb15a3d7e6476b5de978c967d
Coelho753 <165965908+Coelho753@users.noreply.github.com>
2026-03-08 20:15:46 -0300

    Atualizar o paymentRoutes.js

--- src/routes/paymentRoutes.js
+++ src/routes/paymentRoutes.js
@@ -1,12 +1,7 @@
 const express = require("express");
 const router = express.Router();
-
 const paymentController = require("../controllers/paymentController");
 
-/*
-CRIAR PAGAMENTO
-*/
-
-router.post("/create", paymentController.createPayment);
+router.post("/create", paymentController.createPix);
 
 module.exports = router;
